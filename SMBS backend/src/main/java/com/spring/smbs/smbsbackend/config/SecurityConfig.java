ad3d37b6f790acb5fa04ddc3f3920e0ad41603fc
CReshCreates <reshrajchaudhary009@gmail.com>
2026-04-28 14:56:26 +0545

    updated cors config

--- SMBS backend/src/main/java/com/spring/smbs/smbsbackend/config/SecurityConfig.java
+++ SMBS backend/src/main/java/com/spring/smbs/smbsbackend/config/SecurityConfig.java
@@ -1,35 +1,45 @@
-package com.spring.smbs.smbsbackend.config;
+package com.spring.smbs_backend.config;
 
-import com.spring.smbs.smbsbackend.service.MyUserDetailsService;
+import com.spring.smbs_backend.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
     MyUserDetailsService userDetailsService;
 
+    @Autowired
+    JwtFilter jwtFilter;
+
     @Bean
-    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) {
         return http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.requestMatchers("/registerUser", "/login").permitAll().anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .authorizeHttpRequests(request -> request.requestMatchers( "/login").permitAll()
+                        .requestMatchers( "/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/cashier/**").hasRole("CASHIER").
+                        requestMatchers("/product/getByCode/**", "/getCashierById/").hasAnyRole("ADMIN", "CASHIER")
+                        .anyRequest().authenticated())
+
+
+                .formLogin(customizer -> customizer.disable())
+                .httpBasic(basic -> basic.disable()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
