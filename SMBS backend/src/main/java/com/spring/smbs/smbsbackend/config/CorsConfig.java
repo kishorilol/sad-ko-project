4b96edad941cef0bf320e145efc1f87f85d991a0
CReshCreates <reshrajchaudhary009@gmail.com>
2026-04-28 17:46:40 +0545

    Update CorsConfig.java

--- SMBS backend/src/main/java/com/spring/smbs/smbsbackend/config/CorsConfig.java
+++ SMBS backend/src/main/java/com/spring/smbs/smbsbackend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://192.168.1.75:5500", "http://127.0.0.1:5500", "https://sup-bill.netlify.app/"));
+        config.setAllowedOrigins(List.of("http://192.168.1.75:5500", "http://127.0.0.1:5500", "https://sup-bill.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
