ed35788dfb08d34d044c1006b39144917704a62f
CReshCreates <reshrajchaudhary009@gmail.com>
2026-04-29 18:40:28 +0545

    Update CorsConfig.java

--- SMBS backend/src/main/java/com/spring/smbs_backend/config/CorsConfig.java
+++ SMBS backend/src/main/java/com/spring/smbs_backend/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(*));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
