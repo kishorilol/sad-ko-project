f92e50775580d6efa85ec3d76a4dfd4b3e200bac
CReshCreates <reshrajchaudhary009@gmail.com>
2026-04-30 09:09:47 +0545

    updated

--- SMBS backend/src/main/java/com/spring/smbs_backend/config/JwtFilter.java
+++ SMBS backend/src/main/java/com/spring/smbs_backend/config/JwtFilter.java
@@ -30,6 +30,16 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        response.setHeader("Access-Control-Allow-Origin", "https://sup-bill.up.railway.app");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
         if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
             filterChain.doFilter(request, response);
             return;
